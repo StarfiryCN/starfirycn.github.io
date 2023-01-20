faf62654eb2a9ddb54f148f55066e27719762c2c
StarfiryCN <103474377+StarfiryCN@users.noreply.github.com>
2023-01-20 23:54:21 +0800

    Add files via upload

---
+++ pc/pages/characters.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+    var CastorFiberTag = document.getElementById("CastorFiberTag")
+    var MoleTag = document.getElementById("MoleTag")
+    var MouseTag = document.getElementById("MouseTag")
+    var CastorFiber = document.getElementById("CastorFiber")
+    var Mole = document.getElementById("Mole")
+    var Mouse = document.getElementById("Mouse")
+
+    CastorFiberTag.setAttribute("style", "background-color:rgb(170,200,170);border-radius:15px;")
+
+    CastorFiberTag.onmouseover = CastorFiberTag.onmouseout = function () {
+        CastorFiberTag.setAttribute("style", "background-color:rgb(170,200,170);border-radius:15px;")
+        MoleTag.setAttribute("style", "")
+        MouseTag.setAttribute("style", "")
+        CastorFiber.style.display = "flex"
+        Mole.style.display = "none"
+        Mouse.style.display = "none"
+    }
+
+    MoleTag.onmouseover = MoleTag.onmouseout = function () {
+        CastorFiberTag.setAttribute("style", "")
+        MoleTag.setAttribute("style", "background-color:rgb(170,200,170);border-radius:15px;")
+        MouseTag.setAttribute("style", "")
+        CastorFiber.style.display = "none"
+        Mole.style.display = "flex"
+        Mouse.style.display = "none"
+    }
+
+    MouseTag.onmouseover = MouseTag.onmouseout = function () {
+        CastorFiberTag.setAttribute("style", "")
+        MoleTag.setAttribute("style", "")
+        MouseTag.setAttribute("style", "background-color:rgb(170,200,170);border-radius:15px;")
+        CastorFiber.style.display = "none"
+        Mole.style.display = "none"
+        Mouse.style.display = "flex"
+    }
+}
